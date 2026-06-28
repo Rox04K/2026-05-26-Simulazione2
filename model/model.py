8667bd245947e21fb5febe0a47665dc8083844a9
Rox04K <s322647@studenti.polito.it>
2026-06-28 10:30:05 +0200

    Finita la simulazione d'esame

--- model/model.py
+++ model/model.py
@@ -1,3 +1,5 @@
+import copy
+
 import networkx as nx
 
 from database.DAO import DAO
@@ -9,7 +11,7 @@ class Model:
         self._IDMap = {}
 
         self._bestCammino = []
-        self._bestPunteggio = 0
+        self._bestLunghezza = 0
 
     def getRating(self):
         return DAO.getRating()
@@ -39,91 +41,36 @@ class Model:
         largest_cc = max(nx.connected_components(self._grafo), key=len)
         return num, largest_cc
 
-    def getCamminoOttimo(self, nodo):
+    def getCamminoOttimo(self):
         self._bestCammino = []
-        self._bestPunteggio = 0  # float('inf) se devo minimizzare qualcosa
-        # EVENTUALI ALTRE CONDIZIONI CHE POTREBBERO ESSERE UTILI
-
-        # ATTENZIONE! Se vuole che massimizzo la lunghezza del cammino:
         self._bestLunghezza = 0
 
-        # SE HO UN NODO DI PARTENZA
-        parziale = [self._IDMap[nodo]]
-        self._ricorsione(parziale)
-
-        # SE NON HO UN NODO DI PARTENZA
-        for n in self._grafo.neighbors():
+        for n in self._grafo.nodes():
             parziale = [n]
             self._ricorsione(parziale)
 
-        return self._bestCammino, self._bestPunteggio
+        return self._bestCammino, (self._bestLunghezza - 1)
 
-        # ATTENZIONE! Se vuole che massimizzo la lunghezza del cammino, poi ritorno:
-        # return self._bestCammino, (self._bestLunghezza - 1)  #Il -1 ci da il numero di archi
-
-    def _ricorsione(self, parziale, limite):
-        ''' METODO RICORSIVO VERO E PROPRIO, COMPOSTO DA PIù PASSAGGI:
-            - CALCOLO I NODI VALIDI SECONDO LE SPECIFICHE DEL PROBLEMA
-            - CONTROLLO SE HO ANCORA NODI DA VERIFICARE O SE SONO ARRIVATA ALLA FINE DELLA MIA RICORSIONE
-            - CONTROLLO SE LA MIA SOLUZIONE VALIDA è ANCHE OTTIMA E AGGIORNO
-            - CONTROLLO SE RISPETTO DEI VINCOLI (AD ESEMPIO MASSIMO 4 NODI ECC.) ALTRIMENTI return
-            - CICLO SU VALIDI PER APPLICARE LE CONDIZIONI DI RICORSIONE '''
+    def _ricorsione(self, parziale):
 
         validi = self._getSuccessors(parziale)  # Mi calcola i successori del nodo
         if validi == []:
-            pesoAttuale = self._peso(
-                parziale)  # calcolo il nuovo punteggio (oppure se non ho un punteggio non lo metto)
-            if pesoAttuale > self._bestPunteggio:  # se devo massimizzare. Se devo minimizzare metterò <
+            if len(parziale) > self._bestLunghezza:
                 self._bestCammino = copy.deepcopy(parziale)
-                self._bestPunteggio = pesoAttuale
+                self._bestLunghezza = len(parziale)
 
-            ''' ATTENZIONE SE MI CHIEDE IL CAMMINO DI LUNGHEZZA MASSIMA DEVO PRIMA CONTROLLARE OGNI VOLTA CHE È PIù LUNGO
-                E AGGIORNARE, POI POSSO CONTROLLARE IL PESO A PARITà DI LUNGHEZZA'''
-
-        ''' SE HO UN NODO DI ARRIVO DEVO TERMINARE SIA SE ARRIVO AL NODO DI ARRIVO 
-            CHE SE ARRIVO ALLA LUNGHEZZA MASSIMA SENZA AVERE IL NODO DI ARRIVO CHE 
-            VOLEVO'''
-        if parziale[-1].product_id == end:
-            if len(parziale) - 1 == limite:
-                pesoAttuale = self._peso(parziale)
-                if pesoAttuale > self._bestPunteggio:
-                    self._bestCammino = copy.deepcopy(parziale)
-                    self._bestPunteggio = pesoAttuale
-                return
-            else:
-                return
-
-        if len(parziale) - 1 == limite:
-            return
-
-        if len(parziale) == limite:  # eventuale condizione di terminazione
-            # check
-            return
-
-        for n in validi:  # continuo la mia ricorsione
+        for n in validi:
             parziale.append(n)
             self._ricorsione(parziale)
             parziale.pop()
 
     def _getSuccessors(self, parziale):
-        ''' PRENDE TUTTI I VICINI DEL GRAFO E CONTROLLA SE POSSONO ESSERE CONSIDERATI NELLA RICORSIONE'''
-        succ = self._grafo.successors(parziale[-1])  # self._grafo.neighbors() nel caso di Graph()
+        succ = self._grafo.neighbors(parziale[-1])  #
         validi = []
 
         for n in succ:
-            if n not in parziale:  # condizione di non ripetibilita di un nodo (NON VOGLIO CICLI)
-
-                ''' INSERIRE QUI IL CONTROLLO SUI NODI CHE DEVE ESSERE FATTO. ALCUNI ESEMPI:'''
-                # controllo del peso sugli archi:
-                if len(parziale) >= 2:
-                    if self._grafo[parziale[-1]][n]['weight'] < self._grafo[parziale[-2]][parziale[-1]][
-                        'weight']:  # strettamente decrescente
-                        validi.append(n)
-                else:
-                    validi.append(n)
-
-                # controllo su caratteristiche del nodo:
-                if n.duration > parziale[-1].duration:  # strettamente crescente
+            if n not in parziale:
+                if n.date_of_birth > parziale[-1].date_of_birth:
                     validi.append(n)
 
         return validi
